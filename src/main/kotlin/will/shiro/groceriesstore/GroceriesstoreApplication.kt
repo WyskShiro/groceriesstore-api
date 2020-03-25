ebfd633bf2d07f33d892fdd66e85dede0abb1942
William <william@jera.com.br>
2020-03-25 16:47:59 -0400

    Repo init

---
+++ src/main/kotlin/will/shiro/groceriesstore/GroceriesstoreApplication.kt
@@ -0,0 +1,11 @@
+package will.shiro.groceriesstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroceriesstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<GroceriesstoreApplication>(*args)
+}
---
+++ src/test/kotlin/will/shiro/groceriesstore/GroceriesstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package will.shiro.groceriesstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroceriesstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
